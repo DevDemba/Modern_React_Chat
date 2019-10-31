d13f6ba4e7cec5db4f60f4d6da3536ebf274f589
Demba <m.demba.dia@gmail.com>
2019-10-31 19:46:25 +0100

    build

---
+++ modern-react-chat/build/precache-manifest.80b2396042d5b018d0c61792a6428612.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ed4913978a8e3ab211ab974d0d7b06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67aa61c25914116ddf25",
+    "url": "/static/css/main.bf7d9f22.chunk.css"
+  },
+  {
+    "revision": "b694b7f4cbea018f1908",
+    "url": "/static/js/2.748b8b34.chunk.js"
+  },
+  {
+    "revision": "67aa61c25914116ddf25",
+    "url": "/static/js/main.d9071f74.chunk.js"
+  },
+  {
+    "revision": "f5b962750379278bfa32",
+    "url": "/static/js/runtime-main.a8dc52bd.js"
+  }
+]);
